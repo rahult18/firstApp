5c6586926e1af97366893cb3b067b0bb892325f7
Rahul Reddy Talatala <rahul.talatala@gmail.com>
2024-02-19 21:17:22 -0500

    Initial Commit

---
+++ src/main/java/com/spring/firstApp/FirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.firstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+	}
+
+}
